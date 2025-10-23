cb67ffc04ff610f8b0f9f57abd3b219b2d369f0e
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-10-23 21:33:09 +0300

    feat: authcontroller.java implemented.

---
+++ src/main/java/com/enesgunumdogdu/hospital_management/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.enesgunumdogdu.hospital_management.controller;
+
+import com.enesgunumdogdu.hospital_management.domain.dto.AuthResponse;
+import com.enesgunumdogdu.hospital_management.domain.dto.AuthRequest;
+import com.enesgunumdogdu.hospital_management.domain.dto.RegisterRequest;
+import com.enesgunumdogdu.hospital_management.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+
+}
