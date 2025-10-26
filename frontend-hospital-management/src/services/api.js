33e08a292d972d3e73bb3cae3189b4f8452eb4d9
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-10-26 20:13:27 +0300

    feat: add axios interceptors for automatic token management

--- frontend-hospital-management/src/services/api.js
+++ frontend-hospital-management/src/services/api.js
@@ -7,4 +7,33 @@ const api = axios.create({
   }
 })
 
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token')
+      localStorage.removeItem('userData')
+      delete api.defaults.headers.common['Authorization']
+      
+      window.dispatchEvent(new CustomEvent('auth:unauthorized'))
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default api
